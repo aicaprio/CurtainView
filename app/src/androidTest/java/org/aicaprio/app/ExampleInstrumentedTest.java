c962841614c8b7ae4b78a5d8b25c58428c8e192d
Shuo <aicaprio@163.com>
2017-12-12 17:28:35 +0800

    Clean start

--- app/src/androidTest/java/org/aicaprio/app/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/org/aicaprio/app/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("org.aicaprio.app", appContext.getPackageName()); 
+        assertEquals("org.aicaprio.app", appContext.getPackageName());
     }
 }
