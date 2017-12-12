bc3de7952c1820fc7d59e89f12d8441ebc34d305
Shuo <aicaprio@163.com>
2017-12-12 17:24:01 +0800

    Clean start

--- app/src/test/java/org/aicaprio/app/ExampleUnitTest.java
+++ app/src/test/java/org/aicaprio/app/ExampleUnitTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertEquals;
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+ */ 
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
